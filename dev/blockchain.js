d399e30d592d7e07dc30edd06540c6ea546de43f
Admin <somkamol.vasuvudhiroch@exxonmobil.com>
2020-01-16 16:15:59 +0700

    Proof of work function

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -52,4 +52,14 @@ Blockchain.prototype.hashBlock = function(previousBlockHash,currentBlockData, no
 }
 
 
+Blockchain.prototype.proofOfWork = function(previousBlockHash,currentBlockData) {
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData,nonce);
+    while (hash.substring(0, 4) !== '0000') {
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    } 
+    return nonce;
+}
+
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -20,9 +20,11 @@ const currentBlockData = [
         recipient: '3A3F6E462AR8I5',  
     }  
 ];
-const nonce = 100;
-
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce ));
+const nonce = 43237;//bitcoin.proofOfWork(previousBlockHash, currentBlockData);//43237
+console.log(nonce);
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+//console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+//console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce ));
 /*
 bitcoin.createNewBlock(789457,'OIUOEDJETH8754DHKD','78SHNEG45DER56');
 bitcoin.createNewTransaction(100,'ALEXHT845SJ5TKCJ2','JENN5BG5DF6HT8NG9');
