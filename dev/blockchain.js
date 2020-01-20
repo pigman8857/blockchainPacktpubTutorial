0f87723a6199fd321141f582962988387d929ee4
Admin <somkamol.vasuvudhiroch@exxonmobil.com>
2020-01-20 17:45:08 +0700

    Give node blockchain an url and awareness of the other nodes that are inside of our network.

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -4,6 +4,8 @@ class Blockchain{
     constructor(){
         this.chain = [];
         this.pendingTransactions = [];  
+        this.currentNodeUrl = currentNodeUrl;
+        this.networkNodes = [];
         this.createNewBlock(100,'0','0');//genesis block
     }
 
