1326d1b52677d88a072d11621dff4d8cd12be931
Admin <somkamol.vasuvudhiroch@exxonmobil.com>
2020-01-20 16:38:27 +0700

    use the port coming from command

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -53,6 +53,6 @@ app.get('/mine', function(req, res) {
       });
 });
 
-app.listen(3000,function(){
-    console.log('listening on port 3000...'); 
+app.listen(port,function(){
+    console.log(`listening on port ${port}...`); 
 });
