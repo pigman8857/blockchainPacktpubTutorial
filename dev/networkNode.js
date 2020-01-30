75bc214a5cbfacf8fdea147f01115a436b75d495
Admin <somkamol.vasuvudhiroch@exxonmobil.com>
2020-01-30 15:10:57 +0700

    Refacter /transaction enpoint to work with /transaction/broadcast

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -29,8 +29,9 @@ app.get('/blockchain', function (req, res) {
  * This endpoint work after
  */
 app.post('/transaction', function(req, res) {
-    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
-    res.json({ note:`Transaction will be added in block ${blockIndex}.`});
+    const newTransaction = req.body;
+    const blockIndex = bitcoin.addTransactionToPendingTransactions(newTransaction);
+    res.json({ note: `Transaction will be added in block ${blockIndex}.`});
 });
 
 /**
