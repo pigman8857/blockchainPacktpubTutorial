c5fe7135e8b0aecce63f5f495554a6cbd9791e1f
Admin <somkamol.vasuvudhiroch@exxonmobil.com>
2020-02-12 15:50:14 +0700

    Page to explore block and area of improvement text

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -278,6 +278,10 @@ app.get('/address/:address', function(req, res) {
     });
 });
 
+app.get('/block-explorer', function(req, res) {
+    res.sendFile('./block-explorer/index.html', { root: __dirname });
+});
+
 app.listen(port,function(){
     console.log(`listening on port ${port}...`); 
 });
